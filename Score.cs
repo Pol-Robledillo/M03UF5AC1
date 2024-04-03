1279f2fc846bd836a5b6174a51acedcfa54f2781
Pol-Robledillo <146937489+Pol-Robledillo@users.noreply.github.com>
2024-04-03 14:33:37 +0300

    Update Score.cs

--- Score.cs
+++ Score.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace M03UF5AC1
 {
-    public class Score : IComparable<Score>
+    public class Score
     {
         public string? Player { get; set; }
         public string? Mission { get; set; }
@@ -21,39 +21,6 @@ namespace M03UF5AC1
         public override string ToString()
         {
             return $"Player: {Player}, Mission: {Mission}, Score: {ScorePoints}";
-        }
-        public static bool ValidatePlayerFormat(string player)
-        {
-            const string MsgPlayerFormatError = "Player name must contain only letters.";
-            Regex regex = new Regex(@"^[a-zA-Z]+$");
-            if (!regex.IsMatch(player))
-            {
-                Console.WriteLine(MsgPlayerFormatError);
-            }
-            return regex.IsMatch(player);
-        }
-        public static bool ValidateMissionFormat(string mission)
-        {
-            const string MsgMissionFormatError = "Mission name must start with the name of a greek letter followed by a ''-'' and three numbers.";
-            Regex regex = new Regex(@"^(beta|gamma|delta|zeta|theta|kappa|lambda|mu|nu|xi|pi|rho|sigma|tau|phi|chi|psi)-\d{3}$");
-            if (!regex.IsMatch(mission))
-            {
-                Console.WriteLine(MsgMissionFormatError);
-            }
-            return regex.IsMatch(mission);
-        }
-        public static bool ValidateScorePoints(int scorePoints)
-        {
-            const string MsgScorePointsError = "Score points must be between 0 and 500.";
-            if (scorePoints < 0 || scorePoints > 500)
-            {
-                Console.WriteLine(MsgScorePointsError);
-            }
-            return scorePoints >= 0 && scorePoints <= 500;
-        }
-        public int CompareTo(Score? other)
-        {
-            return ScorePoints.CompareTo(other.ScorePoints);
         }
     }
 }
